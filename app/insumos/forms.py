1ec4e672f17687912603de2fa3ee32e490662c18
devwenderson <wend.devdesigner@gmail.com>
2024-12-09 08:24:57 -0300

    feat: adiciona class no campos de form

--- app/insumos/forms.py
+++ app/insumos/forms.py
@@ -3,11 +3,19 @@ from decimal import Decimal, InvalidOperation
 from app.insumos.models import TipoInsumo, Insumo
 
 class InsumoForm(forms.ModelForm):
-    preco = forms.CharField(label='Preço',widget=forms.TextInput(attrs={'class': 'form-control price',}))
+    preco = forms.CharField(label='Preço')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
+        self.fields['preco'].widget.attrs.update({'class': 'form-control price'})
+            
+
+    
     class Meta:
         model = Insumo
         fields = '__all__'
-    
+        
     def clean_preco(self):
         data = self.cleaned_data['preco']
         data = data.replace(',', '.')
@@ -15,8 +23,6 @@ class InsumoForm(forms.ModelForm):
             return Decimal(data)
         except InvalidOperation:
             raise forms.ValidationError("O preço deve ser um número válido.") 
-        
-        
-    
 
+            
         
