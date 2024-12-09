ed2d4da4dd87de03bb207eb6dd462aba48bf3572
devwenderson <wend.devdesigner@gmail.com>
2024-12-09 07:56:54 -0300

    feat: adiciona mask de dinheiro

--- app/insumos/forms.py
+++ app/insumos/forms.py
@@ -1,8 +1,22 @@
 from django import forms
+from decimal import Decimal, InvalidOperation
 from app.insumos.models import TipoInsumo, Insumo
 
 class InsumoForm(forms.ModelForm):
+    preco = forms.CharField(label='Preço',widget=forms.TextInput(attrs={'class': 'form-control price',}))
     class Meta:
         model = Insumo
         fields = '__all__'
+    
+    def clean_preco(self):
+        data = self.cleaned_data['preco']
+        data = data.replace(',', '.')
+        try:
+            return Decimal(data)
+        except InvalidOperation:
+            raise forms.ValidationError("O preço deve ser um número válido.") 
+        
+        
+    
+
         
