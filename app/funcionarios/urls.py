b6a1d6991b6dfeb3cb88eb2549876967a4301ac0
Ana Barbosa <anael.noki@gmail.com>
2024-12-14 20:44:11 -0300

    feat: atualiza urls do sistema

--- app/funcionarios/urls.py
+++ app/funcionarios/urls.py
@@ -12,36 +12,36 @@ from app.funcionarios.views import (
 )
 
 urlpatterns = [
-    path("funcionarios/", FuncionarioListView.as_view(), name="list-funcionarios"),
+    path("", FuncionarioListView.as_view(), name="list-funcionarios"),
     path(
-        "funcionarios/cadastrar",
+        "cadastrar",
         FuncionarioCreateView.as_view(),
         name="create-funcionario",
     ),
     path(
-        "funcionarios/<int:pk>/deletar",
+        "<int:pk>/deletar",
         FuncionarioDeleteView.as_view(),
         name="delete-funcionario",
     ),
     path(
-        "funcionarios/<int:pk>/atualizar/",
+        "<int:pk>/atualizar/",
         FuncionarioUpdateView.as_view(),
         name="update-funcionario",
     ),
     # PRODUÇÃO
-    path("funcionarios/<int:pk>/producoes/", list_producao, name="list-producao"),
+    path("<int:pk>/producoes/", list_producao, name="list-producao"),
     path(
-        "funcionarios/<int:pk>/producoes/cadastrar/",
+        "<int:pk>/producoes/cadastrar/",
         create_producao,
         name="create-producao",
     ),
     path(
-        "funcionarios/<int:pk>/producoes/<int:producao_pk>/atualizar/",
+        "<int:pk>/producoes/<int:producao_pk>/atualizar/",
         update_producao,
         name="update-producao",
     ),
     path(
-        "funcionarios/<int:pk>/producoes/<int:producao_pk>/deletar/",
+        "<int:pk>/producoes/<int:producao_pk>/deletar/",
         delete_producao,
         name="delete-producao",
     ),
--- app/insumos/urls.py
+++ app/insumos/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from app.insumos.views import InsumoListView, InsumoCreateView, InsumoDeleteView, InsumoUpdateView
+from app.insumos.views import (
+    InsumoListView,
+    InsumoCreateView,
+    InsumoDeleteView,
+    InsumoUpdateView,
+)
 
 urlpatterns = [
-    path('insumos/', InsumoListView.as_view(), name='list-insumos'),
-    path('insumo/cadastrar', InsumoCreateView.as_view(), name='create-insumo'),
-    path('insumo/<int:pk>/atualizar/', InsumoUpdateView.as_view(), name='update-insumo'),
-    path('insumo/deletar/<int:pk>/', InsumoDeleteView.as_view(), name='delete-insumo'),
+    path("", InsumoListView.as_view(), name="list-insumos"),
+    path("cadastrar", InsumoCreateView.as_view(), name="create-insumo"),
+    path("<int:pk>/atualizar/", InsumoUpdateView.as_view(), name="update-insumo"),
+    path("deletar/<int:pk>/", InsumoDeleteView.as_view(), name="delete-insumo"),
 ]
